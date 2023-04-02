5b648c20e1850aac5657ee49e100df1f1f49645e
Keigo-Iwakuma <sym90chn@gmail.com>
2023-04-02 22:27:53 +0900

    create simple webapp.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    content: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    let echo = warp::post()
+        .and(warp::path("echo"))
+        .and(warp::body::json())
+        .map(|msg: Message| warp::reply::json(&msg));
+
+    let routes = hello.or(echo);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
